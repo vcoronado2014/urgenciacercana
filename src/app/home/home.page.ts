eef154e7b65592a75ea2e72404835f1c4b945f91
vcoronado2014 <vcoronado.alarcon@gmail.com>
2020-04-28 18:18:37 -0400

    prueba git

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -25,7 +25,7 @@ export class HomePage implements OnInit {
   tokenDispositivo: any;
   subscription: any;
   esIOS = false;
-  //navigator = {app: any};
+  
   objetoEntrada = {
     VersionAppName: '',
     Plataforma: '',
