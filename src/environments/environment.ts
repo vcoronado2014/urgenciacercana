7e3895f17123380985ab6bc8924a6193627af1bb
vcoronado2014 <vcoronado.alarcon@gmail.com>
2020-06-17 22:14:04 -0400

    valores verdaderos y falsos solucionados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,18 +4,19 @@
 
 export const environment = {
   production: false,
-  //API_ENDPOINT: 'https://app.rayensalud.com/MiFamilia/Api/',
-  //API_RAIZ: 'https://app.rayensalud.com/MiFamilia',
+  API_ENDPOINT: 'https://app.rayensalud.com/MiFamilia/Api/',
+  API_RAIZ: 'https://app.rayensalud.com/MiFamilia',
   //http://190.151.14.101:8065/
   //API_ENDPOINT: 'http://10.211.55.5/MiFamilia.WebApi/Api/',
-  API_ENDPOINT: 'http://190.151.14.101:8065/Api/',
-  API_RAIZ: 'http://190.151.14.101:8065',
+  //API_RAIZ: 'http://10.211.55.5/MiFamilia.WebApi',
+  //API_ENDPOINT: 'http://190.151.14.101:8065/Api/',
+  //API_RAIZ: 'http://190.151.14.101:8065',
   viewZoomControl: false,
   viewMapTypeControl: false,
   viewfullscreenControl: false,
   viewstreetViewControl: false,
   //logo e info de la poropaganda
-  imgPropaganda: "./assets/imgs/logo_rayen.png",
+  imgPropaganda: "./assets/imgs/banner_pequeno_rayen.png",
   tituloPropaganda: "Información proporcionada por Rayen Salud",
   subTituloPropaganda: "Email: contacto@rayensalud.com",
   /* API_KEY_MAPA: 'AIzaSyArou1B6ZXJexDEYfI98MDHoq-eSKdhscU', */
